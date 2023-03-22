4b2558afff327e2541f8bd19e6715f5f5f4d3746
adityapermono8 <adityapermono8@gmail.com>
2023-03-22 17:21:31 +0700

    Menambahkan int main

--- pemrogramandasar/pemrogramandasar.cpp
+++ pemrogramandasar/pemrogramandasar.cpp
@@ -12,3 +12,57 @@ float luaslingkaran(float r) {
 float luassegitiga(float a, float t) {
     return 0.5 * a * t;
 }
+
+int main() {
+    float panjang, lebar, jejari, alas, tinggi;
+    int pilihan;
+
+
+    do
+    {
+        cout << "=================" << endl;
+        cout << "=====M E N U======" << endl;
+        cout << "=================" << endl;
+
+        cout << "1. luas persegi panjang" << endl;
+        cout << "2. luas segitiga" << endl;
+        cout << "3. luas lingkaran" << endl;
+        cout << "4 exit" << endl;
+
+        cout << "masukan pilihan" << endl;
+        cout << "\npilihan (1/2/3/4) : ";
+        cin >> pilihan;
+
+
+        switch (pilihan)
+        {
+        case 1:
+            cout << "masukan panjang : ";
+            cin >> panjang;
+            cout << "masukan lebar : ";
+            cin >> lebar;
+            cout << "luas persegipanjang = " << luaspersegipanjang(panjang, lebar);
+            break;
+        case 2:
+            cout << "masukan alas : ";
+            cin >> alas;
+            cout << "masukan tinggi : ";
+            cin >> tinggi;
+            cout << "luas segitiga = " << luassegitiga(alas, tinggi);
+            break;
+        case 3:
+            cout << "masukan jari-jari : ";
+            cin >> jejari;
+            cout << "luas lingkaran : " << luaslingkaran(jejari);
+            break;
+
+        case 4:
+
+            break;
+        default:
+            cout << " pilihan anda salah!";
+            break;
+        }
+    } while (pilihan != 4);
+
+}
